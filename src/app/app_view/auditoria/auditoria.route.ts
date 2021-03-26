63f28f5cb18f0bec0b5d556fdd943605057a124a
Roberto Meneghelli <roberto.meneghelli@triasoftware.com.br>
2021-03-26 12:09:45 -0300

    Versão 2.4 (Adicionado rota para validação)
    Commit (Versão 2.4 (Adicionado rota para validação))

--- src/app/app_view/auditoria/auditoria.route.ts
+++ src/app/app_view/auditoria/auditoria.route.ts
@@ -5,7 +5,11 @@ import { Routes, RouterModule } from '@angular/router';
 import { AuthGuard } from 'src/app/guards/auth.guard';
 
 const routes: Routes = [
-  { path: '', component: ListaAuditoriaComponent, canActivate: [AuthGuard] },
+  {
+    path: '', component: ListaAuditoriaComponent, canActivate: [AuthGuard], data: {
+      allowedRoles: ['ROLE_LOG']
+    }
+  },
   { path: ':id', component: AuditoriaViewComponent, canActivate: [AuthGuard] },
 ];
 
