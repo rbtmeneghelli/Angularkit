1da736a82c3fac869386a5e7d7d2555c13c8f534
Roberto Meneghelli <roberto.mng.89@gmail.com>
2023-11-10 22:50:26 -0300

    Versão 4.4 - Componente de MultCheckBox
    Commit (Versão 4.4 - Componente de MultCheckBox)

---
+++ src/app/shared/generic-mult-checkbox/generic-mult-checkbox.component.ts
@@ -0,0 +1,33 @@
+import { Component, Input } from '@angular/core';
+import { AbstractControl, FormArray, FormControl } from '@angular/forms';
+
+@Component({
+    selector: 'generic-mult-checkbox',
+    templateUrl: './generic-mult-checkbox.component.html',
+    styleUrls: ['./generic-mult-checkbox.component.scss'],
+})
+
+export class GenericMultCheckboxComponent {
+
+    // Para testar, fazer o seguinte passo
+    // checkboxOptions >> ['Pessoa Juridica', 'Pessoa Fisica']
+    // o control e um formControl do FormGroup com valor passado como Array Booleano
+    // [false,true] >> Isso dentro do value do formControl
+    
+    @Input() checkboxOptions: string[] = [];
+    @Input() titulo: string = '';
+    @Input() disabled: boolean = false;
+    @Input() control: AbstractControl = new FormControl([]);
+
+    ngOnInit() { }
+
+    toggleCheckbox(index: number): void {
+        const arr = this.control.value as boolean[];
+        arr[index] = !arr[index];
+        this.control.setValue(arr);
+    }
+
+    getControl(index: number) {
+        return (this.control as FormArray).at(index) as FormControl;
+    }
+}
