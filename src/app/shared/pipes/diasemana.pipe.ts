4b634dbd48f97a781489b1a8626e0d70b81d1815
Roberto Meneghelli <r.grigoragi@avivatec.com.br>
2024-09-27 09:56:33 -0300

    Versão 6.9 - Aplicando Dictionary do C# no frontend
    Commit (Versão 6.9 - Aplicando Dictionary do C# no frontend)

--- src/app/shared/pipes/diasemana.pipe.ts
+++ src/app/shared/pipes/diasemana.pipe.ts
@@ -6,14 +6,15 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 export class DiaSemanaPipe implements PipeTransform {
     transform(value: number): string {
-        const mapa = new Map<number, string>();
-        mapa.set(1, 'Domingo');
-        mapa.set(2, 'Segunda-feira');
-        mapa.set(3, 'Terça-feira');
-        mapa.set(4, 'Quarta-feira');
-        mapa.set(5, 'Quinta-feira');
-        mapa.set(6, 'Sexta-feira');
-        mapa.set(7, 'Sabado');
-        return mapa.get(value);
-    }
+        // dictionary no frontend
+    const dictionary: { [key: number]: string } = {
+        [1]: 'Domingo',
+        [2]: 'Segunda-feira',
+        [3]: 'Terça-feira',
+        [4]: 'Quarta-feira',
+        [5]: 'Quinta-feira',
+        [6]: 'Sexta-feira',
+        [7]: 'Sabado',
+    };
+    return dictionary[value];
 }
